85ac64321c42e8512a38d2e5fef1705d6d44fddb
Quang Huy <2hnono30@gmail.com>
2022-06-03 15:43:08 +0700

    liner equation resolver

---
+++ Javacore/liner-equation-resolver/src/com/codegym/Main.java
@@ -0,0 +1,27 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Liner Equation Resolver");
+        System.out.println("Giver a equation as 'a*x+b=c',please enter constants:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+        System.out.println("c: ");
+        double c = scanner.nextDouble();
+        if (a!=0){
+            double answer= (c-b)/a;
+            System.out.printf("Equation pass with x = %f!\n",answer);
+        }else {
+            if (b==c){
+                System.out.print("The solution all x!");
+            }else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
