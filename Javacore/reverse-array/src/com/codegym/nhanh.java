43b9be2f9368be6622e0768932c5cc47ed00afe0
Quang Huy <2hnono30@gmail.com>
2022-06-07 13:39:38 +0700

    reverse array

--- Javacore/reverse-array/src/com/codegym/nhanh.java
+++ Javacore/reverse-array/src/com/codegym/nhanh.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class nhanh {
     public static void main(String[] args) {
         int[] array = {2, 3, 4, 5, 56, 6, 1, 4};
-            for (int i=0;i<= array.length/2 ; i++) {
+            for (int i=0;i< array.length/2 ; i++) {
                 int temp = array[i];
                 array[i] = array[array.length-1-i];
                  array [array.length-1-i] = temp;
