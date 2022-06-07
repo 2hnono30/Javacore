fd7aa7aaa0c206b634a6cf35499c503eecb5eec0
Quang Huy <2hnono30@gmail.com>
2022-06-07 08:20:47 +0700

    20 số nguyên tố đầu

---
+++ Javacore/snt/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        int status = 1;
+        int num=3;
+        for ( int i = 2 ; i <=100 ;  )
+        {
+            for ( int j = 2 ; j <= Math.sqrt(num) ; j++ )
+            {
+                if ( num%j == 0 )
+                {
+                    status = 0;
+                    break;
+                }
+            }
+            if ( status != 0 )
+            {
+                System.out.println(num);
+                i++;
+                count++;
+            }
+            status = 1;
+            num++;
+            if (count == 20)
+                break;
+        }
+
+    }
+}
