5038123ba90292dbb7a055ce5a24420d5803794d
Quang Huy <2hnono30@gmail.com>
2022-06-08 08:51:04 +0700

    delete element

---
+++ Javacore/delete-element/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input num:");
+        int X = scanner.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==X){
+                arr[i]=arr[i++];
+            }
+            System.out.println(arr[i] + "");
+
+        }
+
+    }
+}
